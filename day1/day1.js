f8455d027a56efb6704b1c431cf56df8f16fb89b
nshah-tech <nshah@nshah.tech>
2022-12-03 21:30:24 +0530

    Day 1

---
+++ day1/day1.js
@@ -0,0 +1,21 @@
+
+import { readFile } from 'fs/promises';
+
+const start = async () => {
+    console.log(process.cwd());
+    const fileData = (await readFile(__dirname + '/input.txt')).toString().split("\n");
+    let calorieArray = [];
+    let currentValue = 0;
+    fileData.forEach((element) => {
+        if(element === '') {
+            calorieArray.push(Number(currentValue))
+            currentValue = 0;
+        } else {
+            currentValue += Number(element);
+        }
+    })
+    calorieArray = calorieArray.sort((a,b)=>b-a);
+    console.log('Elf with highest calorie = ', calorieArray[0])
+    console.log('Sum of top 3 Elf backpack = ', calorieArray[0] + calorieArray[1] + calorieArray[2] )
+}
+start();
